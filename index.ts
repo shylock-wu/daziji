c9d727bb8bd96f3ccc5ce276a52907b52c17480d
shylock <shylock.wu@moblor.com>
2023-06-25 14:18:34 +0800

    first commit

--- index.ts
+++ index.ts
@@ -7,6 +7,25 @@ const server = createServer((request, response) => {
     const stream = fs.createReadStream('./index.html');
     stream.pipe(response);
   } else if (url.pathname === '/sse') {
+    response.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive'
+    });
+    // 创建文件读取流
+    const readStream = fs.createReadStream('test.txt', {
+      encoding: 'utf-8',
+      highWaterMark: 1 // 每次读取一个字母
+    });
+    // 监听数据事件
+    readStream.on('data', (chunk) => {
+      // 将每个字母作为一个事件发送到客户端
+      console.log('读取');
+      response.write(`event:exampleSSE\ndata: ${chunk}\n\n`);
+      if (!chunk) {
+        response.end();
+      }
+    });
   } else {
     response.end('');
   }
@@ -16,17 +35,24 @@ server.listen('3000', () => {
   console.log('http://127.0.0.1:3000');
 });
 
-const wss = new WebSocket.Server({ port: 3001 });
+const ws = new WebSocket.Server({ port: 3001 });
 
-wss.on('connection', function connection(ws) {
+ws.on('connection', function (socket) {
   console.log('A client connected.');
 
-  ws.on('message', function incoming(message) {
-    console.log(`Received message: ${message}`);
-    // 在这里可以对客户端发送的消息进行处理
+  socket.on('message', function (message) {
+    if (message.toString() === '打字机') {
+      const readStream = fs.createReadStream('./test.txt', 'utf-8');
+      readStream.on('data', (data) => {
+        socket.send(data);
+      });
+      readStream.on('close', () => {
+        socket.close();
+      });
+    }
   });
 
-  ws.on('close', function onClose() {
+  socket.on('close', function () {
     console.log('Client disconnected.');
   });
 });
